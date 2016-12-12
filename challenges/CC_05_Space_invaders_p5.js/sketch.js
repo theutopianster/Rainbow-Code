09056878e657f539782fbd40f2746c9766cdef7b
theutopianster <theutopianster@gmail.com>
2016-12-12 16:06:07 +0200

    Cleanup and collision detections
    fix flowers side margin hit
    collision detection ship hits flowers
    remove drops that are outside canvas
    remove exploded flowers

--- challenges/CC_05_Space_invaders_p5.js/sketch.js
+++ challenges/CC_05_Space_invaders_p5.js/sketch.js
@@ -12,7 +12,7 @@ function setup() {
   ship = new Ship();
   // drop = new Drop(width/2, height/2);
   for (var i = 0; i < 6; i++) {
-    flowers[i] = new Flower(i*80+80, 60);
+    flowers[i] = new Flower(i*80+80, 40);
   }
 }
 
@@ -24,37 +24,79 @@ function draw() {
   for (var i = 0; i < drops.length; i++) {
     drops[i].show();
     drops[i].move();
+
     for (var j = 0; j < flowers.length; j++) {
       if (drops[i].hits(flowers[j])) {
-        flowers[j].grow();
+        flowers[j].takeHit();
         drops[i].evaporate();
       }
     }
   }
 
-  var edge = false;
+  collisionDetection();
+  cleanupDrops();
+  cleanupFlowers();
 
+}
+
+function collisionDetection()
+{
+  var edge = false;
+  var bottomEdge = false;
+  var hitShip = false;
   for (var i = 0; i < flowers.length; i++) {
     flowers[i].show();
     flowers[i].move();
-    if (flowers[i].x > width || flowers[i].x < 0) {
+    if (flowers[i].x >= (width - flowers[i].r) || flowers[i].x <= (0 + flowers[i].r)) {
       edge = true;
+    } else if (flowers[i].y >= (height - flowers[i].r)) {
+      bottomEdge = true;
+    }
+    if (ship.hits(flowers[i])) {
+      hitShip = true;
     }
   }
 
+  if (hitShip) {
+    for (var i = 0; i < flowers.length; i++) {
+      flowers[i].freeze();
+    }
+    ship.explode();
+  }
   if (edge) {
     for (var i = 0; i < flowers.length; i++) {
       flowers[i].shiftDown();
     }
   }
+  if (bottomEdge) {
+    for (var i = 0; i < flowers.length; i++) {
+      flowers[i].toDelete = true;
+    }
+  }
+}
 
+function cleanupDrops()
+{
   for (var i = drops.length-1; i >= 0; i--) {
-    if (drops[i].toDelete) {
+    if (drops[i].toDelete || drops[i].y < 0 ) {
       drops.splice(i, 1);
     }
   }
+}
 
+function cleanupFlowers()
+{
+  for (var i = flowers.length-1; i >= 0; i--) {
+    if (flowers[i].toDelete) {
+      flowers.splice(i, 1);
+    }
+  }
+}
 
+function fireDrop()
+{
+  var drop = new Drop(ship.x, height-ship.h);
+  drops.push(drop);
 }
 
 function keyReleased() {
@@ -66,8 +108,7 @@ function keyReleased() {
 
 function keyPressed() {
   if (key === ' ') {
-    var drop = new Drop(ship.x, height);
-    drops.push(drop);
+    fireDrop();
   }
 
   if (keyCode === RIGHT_ARROW) {
