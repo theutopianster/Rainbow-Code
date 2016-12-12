226e108df2a289483df6693c5d1ffcb7566d8078
theutopianster <theutopianster@gmail.com>
2016-12-12 16:01:21 +0200

    grow and explode after some dropplets

--- challenges/CC_05_Space_invaders_p5.js/flower.js
+++ challenges/CC_05_Space_invaders_p5.js/flower.js
@@ -7,11 +7,26 @@ function Flower(x, y) {
   this.x = x;
   this.y = y;
   this.r = 30;
-
+  this.c = color(255, 0, 200, 150);
+  this.toDelete = false;
   this.xdir = 1;
+  this.hits = 0;
 
   this.grow = function() {
     this.r = this.r + 2;
+  }
+
+  this.takeHit = function() {
+    this.hits++;
+    if (this.hits < 4) {
+      this.grow();
+      this.c = color(50, 100, 250, 150);
+    }
+    if (this.hits > 3) {
+      this.grow();
+      this.c = color('yellow');
+      this.toDelete = true;
+    }
   }
 
   this.shiftDown = function() {
@@ -23,9 +38,14 @@ function Flower(x, y) {
     this.x = this.x + this.xdir;
   }
 
+  this.freeze = function() {
+    this.xdir = 0;
+    console.log('freezing');
+  }
+
   this.show = function() {
     noStroke();
-    fill(255, 0, 200, 150);
+    fill(this.c);
     ellipse(this.x, this.y, this.r*2, this.r*2);
   }
 
