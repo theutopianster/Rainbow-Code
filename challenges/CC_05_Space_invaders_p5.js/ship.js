a1769cf1faa9cb97a45367b23579ea818eb05de0
theutopianster <theutopianster@gmail.com>
2016-12-12 16:02:37 +0200

    Limit side movement to canvas

--- challenges/CC_05_Space_invaders_p5.js/ship.js
+++ challenges/CC_05_Space_invaders_p5.js/ship.js
@@ -4,21 +4,43 @@
 // Code for: https://youtu.be/biN3v3ef-Y0
 
 function Ship() {
-  this.x = width/2;
   this.xdir = 0;
+  this.w = 20;
+  this.h = 60;
+  this.x = width/2;
+  this.y = height-this.w;
+  this.c = color(255);
 
   this.show = function() {
-    fill(255);
+    fill(this.c);
     rectMode(CENTER);
-    rect(this.x, height-20, 20, 60);
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  this.explode = function() {
+    this.c = color('orange');
   }
 
   this.setDir = function(dir) {
     this.xdir = dir;
   }
 
+  this.hits = function(flower) {
+    var d = dist(this.x, this.y, flower.x, flower.y);
+    if (d < flower.r + this.h) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   this.move = function(dir) {
-    this.x += this.xdir*5;
+    if (this.xdir == -1 && this.x >= this.w) {
+      this.x += this.xdir * 5;
+    }
+    if (this.xdir == 1 && this.x <= (width-this.w)) {
+      this.x += this.xdir * 5;
+    }
   }
 
 }
